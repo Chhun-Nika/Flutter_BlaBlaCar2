f215281fb24391ab138cde1afaf9e450cf89ac69
Chhun-Nika <chhunnika488@gmail.com>
2026-03-16 23:39:57 +0700

    BLA-203 – Ride Preferences Global state

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -5,15 +5,21 @@ import 'package:blabla/data/repository/ride/ride_repository_mock.dart';
 import 'package:blabla/data/repository/ride_preference/ride_preference_repository.dart';
 import 'package:blabla/data/repository/ride_preference/ride_preference_repository_mock.dart';
 import 'package:blabla/main_common.dart';
+import 'package:blabla/ui/states/ride_pref_states.dart';
 import 'package:provider/provider.dart';
 
 List<InheritedProvider> get devProvider {
+  final ridePreferenceRepository = RidePreferenceRepositoryMock();
   // inject repository
   return [
     Provider<LocationRepository>(create: (_) => LocationRepositoryMock()),
     Provider<RideRepository>(create: (_) => RideRepositoryMock()),
     Provider<RidePreferenceRepository>(
       create: (_) => RidePreferenceRepositoryMock(),
+    ),
+    ChangeNotifierProvider<RidePrefStates>(
+      create: (_) =>
+          RidePrefStates(ridePreferenceRepository: ridePreferenceRepository),
     ),
   ];
 }
---
+++ lib/ui/states/ride_pref_states.dart
@@ -0,0 +1,25 @@
+import 'package:blabla/data/repository/ride_preference/ride_preference_repository.dart';
+import 'package:blabla/model/ride_pref/ride_pref.dart';
+import 'package:flutter/material.dart';
+
+class RidePrefStates extends ChangeNotifier {
+  final RidePreferenceRepository ridePreferenceRepository;
+  RidePreference? _selectedPreference;
+
+  RidePrefStates({required this.ridePreferenceRepository});
+
+  Future<List<RidePreference>> getRidePrefHistory() async {
+    return await ridePreferenceRepository.getPrefHistory();
+  }
+
+  Future<void> selectPreference(RidePreference preference) async {
+    if (preference != _selectedPreference) {
+      // Set the selected preference
+      _selectedPreference = preference;
+
+      // Push to history
+      await ridePreferenceRepository.addRidePrefToHistory(preference);
+      notifyListeners();
+    }
+  }
+}
